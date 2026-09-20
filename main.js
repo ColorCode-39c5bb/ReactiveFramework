cad2f889d944da3be19dd57cfbd073fd1e7a7ad7
VioletGarden <mengleyin979@gmail.com>
2026-09-20 18:58:08 +0800

    0.2.1

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 HTMLElement._render = function(render){
-	return function(rd, render_acquired, construction){
+	return function(rd, render_acquired){
 		this.rd_torender ??= [];
 		this.rd_torender.push(rd);
 		if(this.__proto__.constructor == HTMLElement) return; //排除还没有升级的自定义元素
 		if(!this.isConnected) return;
 
-		this.reactivedata ??= new DataReactive(construction); //自定义元素在初始化时已确保，此句仅作用于内置元素
+		this.reactivedata ??= new DataReactive(); //自定义元素在初始化时已确保，此句仅作用于内置元素
 		render.call(this, this.reactivedata.merge(this.rd_torender), render_acquired);
 		this.rd_torender = undefined;
 	};
@@ -25,6 +25,8 @@ HTMLElement.prototype.initShadowRoot = function(){
 	this.reactivedata = this.RDCLASS.call(Reflect.construct(DataReactive, [], this.RDCLASS));
 	this.shadowRoot.adoptedStyleSheets.push(defaultStyleSheet);
 	this.shadowRoot.appendChild(this.template.content.cloneNode(true));
+	this.shadowRoot.querySelectorAll(".fold-trigger").forEach(btn=>btn.addEventListener("click", (e)=>btn.parentElement.classList.toggle("fold")));
+	this.shadowRoot.querySelectorAll(".fold-trigger").forEach(btn=>btn.parentElement.classList.add("fold"));
 }
 
 HTMLElement.prototype.reactiverender = HTMLElement._render(function(rd_merged, render_acquired){
@@ -33,7 +35,7 @@ HTMLElement.prototype.reactiverender = HTMLElement._render(function(rd_merged, r
 	render_acquired.call(this, rd_merged);
 });
 
-HTMLElement.prototype.reactiverender_for = function(rdarray, render_acquired, construction){
+HTMLElement.prototype.reactiverender_for = function(rdarray, render_acquired){
 	//if(!rdarray) throw new Error("rdarray必须是数组, 否则此方法不应该有机会调用");
 	if(rdarray === undefined) return;
 	this.Ns_active ??= [this];
@@ -47,7 +49,7 @@ HTMLElement.prototype.reactiverender_for = function(rdarray, render_acquired, co
 			this.container.appendChild(next);
 			this.Ns_active.push(next);
 		}
-		next.reactiverender(rdarray[i], render_acquired, construction);
+		next.reactiverender(rdarray[i], render_acquired);
 	}
 	for(let j=this.Ns_active.length-rdarray.length; j>0; j--){
 		const item = this.Ns_active.pop();
@@ -57,8 +59,8 @@ HTMLElement.prototype.reactiverender_for = function(rdarray, render_acquired, co
 };
 
 
-function DataReactive(construction){
-	Object.defineProperty(this, "construction", {value: construction||{i_f: false, isprimary: true}, enumerable: false, writable: true});
+function DataReactive(){
+	Object.defineProperty(this, "construction", {value: {i_f: false, isprimary: true}, enumerable: false, writable: true});
 	return this;
 }
 
@@ -110,13 +112,13 @@ import NotSlotted from "./component/NotSlotted.js";
 import AppMain from "./component/AppMain.js";
 window.TRUE = {value: true, enumerable: false};
 window.FALSE = {value: false, enumerable: false};
-window.constructor_withTemplate = [];
+window.constructor_customelement = [];
 //window.router = new Router(config_route);
 //window.router.push("/home");
 Promise.all(requestCache.values()).then(()=>{
 	const appmain = document.getElementById("appmain");
 	appmain.remove();
-	constructor_withTemplate.forEach((C)=>{
+	constructor_customelement.forEach((C)=>{
 		C.prototype.reactiverender = HTMLElement._render(C.prototype.reactiverender);
 		C.prototype.connectedCallback = HTMLElement._connectedcallback(C.prototype.connectedCallback);
 		if(C.prototype.RDCLASS) Object.setPrototypeOf(C.prototype.RDCLASS.prototype, DataReactive.prototype);
